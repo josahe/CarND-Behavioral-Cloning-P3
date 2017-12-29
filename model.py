5d4cfd735043faa99bedbd21b2af50d4bbe7cd7b
josahe <josahe@users.noreply.github.com>
2017-12-29 19:03:40 +0000

    added histogram flattening

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import csv
 import cv2
 import numpy as np
 import sklearn
+import random
 
 def open_csv_file(csv_file):
     lines=[]
@@ -11,6 +12,50 @@ def open_csv_file(csv_file):
             lines.append(line)
     lines = lines[1:]
     return lines
+
+def histogram(samples):
+    bins=[0,0,0,0,0,0,0,0,0,0]
+    for line in samples:
+        val = float(line[3])
+        if val >= -1. and val <=-0.8:
+            bins[0]+=1
+        elif val > -0.8 and val <=-0.6:
+            bins[1]+=1
+        elif val > -0.6 and val <=-0.4:
+            bins[2]+=1
+        elif val > -0.4 and val <=-0.2:
+            bins[3]+=1
+        elif val > -0.2 and val <=0.:
+            bins[4]+=1
+        elif val > 0. and val <=0.2:
+            bins[5]+=1
+        elif val > 0.2 and val <=0.4:
+            bins[6]+=1
+        elif val > 0.4 and val <=0.6:
+            bins[7]+=1
+        elif val > 0.6 and val <=0.8:
+            bins[8]+=1
+        elif val > 0.8 and val <=1.:
+            bins[9]+=1
+    print(bins)
+
+def flatten_histogram(samples):
+    bins=[0,0,0,0,0,0,0,0,0,0]
+    new_samples=[]
+    for line in samples:
+        val = float(line[3])
+        if ((val >= -1. and val <= -0.8 and random.random() < 1. ) or
+            (val > -0.8 and val <= -0.6 and random.random() < 0.9) or
+            (val > -0.6 and val <= -0.4 and random.random() < 0.8) or
+            (val > -0.4 and val <= -0.2 and random.random() < 0.5) or
+            (val > -0.2 and val <= 0.   and random.random() < 0.2) or
+            (val > 0.   and val <= 0.2  and random.random() < 0.2) or
+            (val > 0.2  and val <= 0.4  and random.random() < 0.5) or
+            (val > 0.4  and val <= 0.6  and random.random() < 0.8) or
+            (val > 0.6  and val <= 0.8  and random.random() < 0.9) or
+            (val > 0.8  and val <= 1.   and random.random() < 1. )):
+            new_samples.append(line)
+    return new_samples
 
 def import_images(lines, path):
     images=[]
@@ -23,7 +68,7 @@ def import_images(lines, path):
             image = cv2.imread(current_path)
             images.append(image)
             measurement = float(line[3])
-            correction = 0.2
+            correction = 0.28
             if   i == 0:
                 measurements.append(measurement)
             elif i == 1:
@@ -56,8 +101,14 @@ def generator(samples, batch_size=32):
 
 samples = open_csv_file('../car-sim-data/driving_log.csv')
 
+
+histogram(samples)
+samples = flatten_histogram(samples)
+histogram(samples)
+
+
 from sklearn.model_selection import train_test_split
-train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+train_samples, validation_samples = train_test_split(samples, test_size=0.1)
 
 
 train_generator = generator(train_samples)
@@ -68,8 +119,10 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
+from keras.optimizers import Adam
 
 model = Sequential()
+
 model.add(Lambda(lambda x: x / 255.0 - 0.5,
     input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70, 25), (0, 0))))
@@ -79,22 +132,14 @@ model.add(Convolution2D(48,5,5,activation='relu',subsample=(2,2)))
 model.add(Convolution2D(64,3,3,activation='relu'))
 model.add(Convolution2D(64,3,3,activation='relu'))
 model.add(Flatten())
-model.add(Dense(1164))
 model.add(Dense(100))
 model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 
-#len_train_samples_zero = 0
-#len_train_samples_nonzero = 0
-#for line in samples:
-#    if float(line[3]) == 0.:
-#        len_train_samples_zero+=1
-#    else:
-#        len_train_samples_nonzero+=1
-#len_train_samples = 3*2*len_train_samples_zero + 3*2*len_train_samples_nonzero
+adam = Adam(lr=0.0001)
 
-model.compile(loss='mse', optimizer='adam')
+model.compile(loss='mse', optimizer=adam)
 model.fit_generator(train_generator, samples_per_epoch=3*2*len(train_samples),
     validation_data=validation_generator, nb_val_samples=3*2*len(validation_samples),
     nb_epoch=7)
