e98d6dd7325b41ae4b586de4bd0092c6a4187874
josahe <josahe@users.noreply.github.com>
2017-12-29 17:55:42 +0000

    improved model, more images

--- model.py
+++ model.py
@@ -1,50 +1,104 @@
-import pickle
+import csv
+import cv2
 import numpy as np
-import tensorflow as tf
-tf.python.control_flow_ops = tf
+import sklearn
 
-with open('small_train_traffic.p', mode='rb') as f:
-    data = pickle.load(f)
+def open_csv_file(csv_file):
+    lines=[]
+    with open(csv_file) as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            lines.append(line)
+    lines = lines[1:]
+    return lines
 
-    X_train, y_train = data['features'], data['labels']
+def import_images(lines, path):
+    images=[]
+    measurements=[]
+    for line in lines:
+        for i in range(3):
+            source_path = line[i]
+            filename = source_path.split('/')[-1]
+            current_path = path + filename
+            image = cv2.imread(current_path)
+            images.append(image)
+            measurement = float(line[3])
+            correction = 0.2
+            if   i == 0:
+                measurements.append(measurement)
+            elif i == 1:
+                measurements.append(measurement + correction)
+            elif i == 2:
+                measurements.append(measurement - correction)
+    return images, measurements
 
-    from keras.models import Sequential
-    from keras.layers.core import Dense, Activation, Flatten, Dropout
-    from keras.layers.convolutional import Convolution2D
-    from keras.layers.pooling import MaxPooling2D
+def augment_images(in_images, in_measurements, out_images, out_measurements):
+    for image, measurement in zip(in_images, in_measurements):
+        out_images.append(image)
+        out_measurements.append(measurement)
+        #if measurement != 0.: # dont flip images with zero steering angle
+        out_images.append(cv2.flip(image,1))
+        out_measurements.append(measurement*-1.0)
 
-    model = Sequential()
-    model.add(Convolution2D(32, 3, 3, input_shape=(32, 32, 3)))
-    model.add(MaxPooling2D((2, 2)))
-    model.add(Dropout(0.5))
-    model.add(Activation('relu'))
-    model.add(Flatten())
-    model.add(Dense(128))
-    model.add(Activation('relu'))
-    model.add(Dense(5))
-    model.add(Activation('softmax'))
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1:
+        for offset in range(0, num_samples, batch_size):
+            aug_images=[]
+            aug_angles=[]
+            batch_samples = samples[offset : offset + batch_size]
+            images, angles = import_images(batch_samples, '../car-sim-data/IMG/')
+            augment_images(images, angles, aug_images, aug_angles)
+            X_train = np.array(aug_images)
+            y_train = np.array(aug_angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
 
-    X_normalized = np.array(X_train / 255.0 - 0.5 )
 
-    from sklearn.preprocessing import LabelBinarizer
-    label_binarizer = LabelBinarizer()
-    y_one_hot = label_binarizer.fit_transform(y_train)
+samples = open_csv_file('../car-sim-data/driving_log.csv')
 
-    model.compile('adam', 'categorical_crossentropy', ['accuracy'])
-    history = model.fit(X_normalized, y_one_hot, nb_epoch=10, validation_split=0.2)
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
-    with open('small_test_traffic.p', 'rb') as f:
-        data_test = pickle.load(f)
 
-        X_test = data_test['features']
-        y_test = data_test['labels']
+train_generator = generator(train_samples)
+validation_generator = generator(validation_samples)
 
-        X_normalized_test = np.array(X_test / 255.0 - 0.5 )
-        y_one_hot_test = label_binarizer.fit_transform(y_test)
 
-        metrics = model.evaluate(X_normalized_test, y_one_hot_test)
-        for metric_i in range(len(model.metrics_names)):
-            metric_name = model.metrics_names[metric_i]
-                metric_value = metrics[metric_i]
-                    print('{}: {}'.format(metric_name, metric_value))
-                        
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+
+model = Sequential()
+model.add(Lambda(lambda x: x / 255.0 - 0.5,
+    input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+model.add(Convolution2D(24,5,5,activation='relu',subsample=(2,2)))
+model.add(Convolution2D(36,5,5,activation='relu',subsample=(2,2)))
+model.add(Convolution2D(48,5,5,activation='relu',subsample=(2,2)))
+model.add(Convolution2D(64,3,3,activation='relu'))
+model.add(Convolution2D(64,3,3,activation='relu'))
+model.add(Flatten())
+model.add(Dense(1164))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+
+#len_train_samples_zero = 0
+#len_train_samples_nonzero = 0
+#for line in samples:
+#    if float(line[3]) == 0.:
+#        len_train_samples_zero+=1
+#    else:
+#        len_train_samples_nonzero+=1
+#len_train_samples = 3*2*len_train_samples_zero + 3*2*len_train_samples_nonzero
+
+model.compile(loss='mse', optimizer='adam')
+model.fit_generator(train_generator, samples_per_epoch=3*2*len(train_samples),
+    validation_data=validation_generator, nb_val_samples=3*2*len(validation_samples),
+    nb_epoch=7)
+
+
+model.save('model.h5')
+exit()
